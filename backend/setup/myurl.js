16c24818647461ab31ec7dd5e456812187186a94
Aditya kumar <134756409+Adityacode-hub@users.noreply.github.com>
2026-06-12 19:07:07 +0530

    Delete backend/setup/myurl.js

--- backend/setup/myurl.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURL: "mongodb://Adityadb_user:Aditya123@ac-uuwv8ap-shard-00-00.jnzzxci.mongodb.net:27017,ac-uuwv8ap-shard-00-01.jnzzxci.mongodb.net:27017,ac-uuwv8ap-shard-00-02.jnzzxci.mongodb.net:27017/?ssl=true&replicaSet=atlas-zuwpuk-shard-0&authSource=admin&appName=Stackoverflow",
-    secret: "mystrongsecret"
-}
